86d40ca8ea09b8c800973724e5f6128658b3d4f7
onlinecoder789 <online782427@gmail.com>
2022-04-17 07:29:23 +0000

    Mandatory Setup Completed

--- android/app/src/main/kotlin/com/u6amtech/flutter_grocery/MainActivity.kt
+++ android/app/src/main/kotlin/com/u6amtech/flutter_grocery/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.u6amtech.flutter_grocery
+package com.tarchia.foodholic
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/utill/app_constants.dart
+++ lib/utill/app_constants.dart
@@ -2,9 +2,9 @@ import 'package:flutter_grocery/data/model/response/language_model.dart';
 import 'images.dart';
 
 class AppConstants {
-  static const String APP_NAME = 'GroFresh';
-  static const double APP_VERSION = 4.2;
-  static const String BASE_URL = 'https://grofresh-admin.6amtech.com';
+  static const String APP_NAME = 'FoodHolic';
+  static const double APP_VERSION = 1.0;
+  static const String BASE_URL = 'https://admin.foodholic.online';
   static const String CONFIG_URI = '/api/v1/config';
   static const String BANNER_URI = '/api/v1/banners';
   static const String CATEGORY_URI = '/api/v1/categories';
@@ -33,31 +33,41 @@ class AppConstants {
   static const String CUSTOMER_INFO_URI = '/api/v1/customer/info';
   static const String UPDATE_PROFILE_URI = '/api/v1/customer/update-profile';
   static const String ADDRESS_LIST_URI = '/api/v1/customer/address/list';
-  static const String REMOVE_ADDRESS_URI = '/api/v1/customer/address/delete?address_id=';
+  static const String REMOVE_ADDRESS_URI =
+      '/api/v1/customer/address/delete?address_id=';
   static const String ADD_ADDRESS_URI = '/api/v1/customer/address/add';
   static const String UPDATE_ADDRESS_URI = '/api/v1/customer/address/update/';
   static const String ORDER_LIST_URI = '/api/v1/customer/order/list';
   static const String ORDER_CANCEL_URI = '/api/v1/customer/order/cancel';
-  static const String ORDER_DETAILS_URI = '/api/v1/customer/order/details?order_id=';
+  static const String ORDER_DETAILS_URI =
+      '/api/v1/customer/order/details?order_id=';
   static const String TRACK_URI = '/api/v1/customer/order/track?order_id=';
   static const String PLACE_ORDER_URI = '/api/v1/customer/order/place';
-  static const String LAST_LOCATION_URI = '/api/v1/delivery-man/last-location?order_id=';
+  static const String LAST_LOCATION_URI =
+      '/api/v1/delivery-man/last-location?order_id=';
   static const String TIMESLOT_URI = '/api/v1/timeSlot';
   static const String TOKEN_URI = '/api/v1/customer/cm-firebase-token';
-  static const String UPDATE_METHOD_URI = '/api/v1/customer/order/payment-method';
+  static const String UPDATE_METHOD_URI =
+      '/api/v1/customer/order/payment-method';
   static const String REVIEW_URI = '/api/v1/products/reviews/submit';
-  static const String DELIVER_MAN_REVIEW_URI = '/api/v1/delivery-man/reviews/submit';
+  static const String DELIVER_MAN_REVIEW_URI =
+      '/api/v1/delivery-man/reviews/submit';
   static const String DISTANCE_MATRIX_URI = '/api/v1/mapapi/distance-api';
-  static const String SEARCH_LOCATION_URI = '/api/v1/mapapi/place-api-autocomplete';
+  static const String SEARCH_LOCATION_URI =
+      '/api/v1/mapapi/place-api-autocomplete';
   static const String PLACE_DETAILS_URI = '/api/v1/mapapi/place-api-details';
   static const String GEOCODE_URI = '/api/v1/mapapi/geocode-api';
   static const String EMAIL_SUBSCRIBE_URI = '/api/v1/subscribe-newsletter';
 
   //MESSAGING
-  static const String GET_DELIVERYMAN_MESSAGE_URI = '/api/v1/customer/message/get-order-message';
-  static const String GET_ADMIN_MESSAGE_URL = '/api/v1/customer/message/get-admin-message';
-  static const String SEND_MESSAGE_TO_ADMIN_URL = '/api/v1/customer/message/send-admin-message';
-  static const String SEND_MESSAGE_TO_DELIVERY_MAN_URL = '/api/v1/customer/message/send/customer';
+  static const String GET_DELIVERYMAN_MESSAGE_URI =
+      '/api/v1/customer/message/get-order-message';
+  static const String GET_ADMIN_MESSAGE_URL =
+      '/api/v1/customer/message/get-admin-message';
+  static const String SEND_MESSAGE_TO_ADMIN_URL =
+      '/api/v1/customer/message/send-admin-message';
+  static const String SEND_MESSAGE_TO_DELIVERY_MAN_URL =
+      '/api/v1/customer/message/send/customer';
 
   // Shared Key
   static const String THEME = 'theme';
@@ -73,7 +83,10 @@ class AppConstants {
   static const String ON_BOARDING_SKIP = 'on_boarding_skip';
 
   static List<LanguageModel> languages = [
-    LanguageModel(imageUrl: Images.england_flag, languageName: 'English', countryCode: 'US', languageCode: 'en'),
-    LanguageModel(imageUrl: Images.arabic_flag, languageName: 'Arabic', countryCode: 'SA', languageCode: 'ar'),
+    LanguageModel(
+        imageUrl: Images.england_flag,
+        languageName: 'English',
+        countryCode: 'US',
+        languageCode: 'en'),
   ];
 }
